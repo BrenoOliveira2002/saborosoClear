d7754ed7fe30a4598d2c6b04a2994902aa8770a8
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-06 11:05:52 -0300

    connecting nodejs with mysql

---
+++ inc/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'user',
+    database: 'saboroso',
+    password: 'clear2022'
+
+})
+
+module.exports = connection
