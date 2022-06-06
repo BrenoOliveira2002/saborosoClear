5f8b289e0c3aed70202678946633cc57b77a3dc3
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-06 11:34:16 -0300

    EJS include

--- inc/db.js
+++ inc/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
 
-    host: 'localhost',
+    host:'localhost',
     user: 'user',
     database: 'saboroso',
     password: 'clear2022'
