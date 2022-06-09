6781e398c4a432822d72415043c80beb8d17ce48
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-09 09:05:54 -0300

    formatting dates with moment.js and ejs

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,10 @@ var users = require("./../inc/users")
 var admin = require("./../inc/admin");
 var menus = require("./../inc/menus")
 var reservations = require("./../inc/reservations")
+var moment = require("moment")
 var router = express.Router();
+
+moment.locale('pt-BR')
 
 router.user(function(req, res, next){
 
@@ -128,8 +131,9 @@ router.get("/reservations", function(req, res, next){
 
     res.render("admin/reservations", admin.getParams(req, {
         date: {},
-        data
-         
+        data,
+        moment
+
     }))
 })
 })
